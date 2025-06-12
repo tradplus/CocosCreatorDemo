dbabe6510aab83df8d2ae5396220bdb0e8b966ce
tradplus <58809719+tradplus@users.noreply.github.com>
2025-06-12 14:54:17 +0800

    移除外部ios配置

--- iOS/TradPlusSDK/TradplusSdkPlugin.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface TradplusSdkPlugin : NSObject
-
-+ (void)callbackWithEventName:(NSString *)name adUnitID:(NSString *)adUnitID adInfo:(NSDictionary *)adInfo error:(NSError *)error;
-
-+ (void)callbackWithEventName:(NSString *)name adUnitID:(NSString *)adUnitID adInfo:(NSDictionary *)adInfo error:(NSError *)error exp:(NSDictionary *)exp;
-@end
